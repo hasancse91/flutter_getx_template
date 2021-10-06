60eff14bee0b2aa9a6cc487f5e6549fbfdddf3cb
MD Hasibul Hasan Shuvo <hasibul.hasan@brainstation-23.com>
2021-10-06 14:07:18 +0600

    User callDataService to call repository method.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_getx_template/app/core/base/base_controller.dart';
 import 'package:flutter_getx_template/app/core/model/github_search_query_param.dart';
+import 'package:flutter_getx_template/app/data/model/github_repo_search_response.dart';
 import 'package:flutter_getx_template/app/data/repository/github_repository.dart';
 import 'package:flutter_getx_template/app/modules/home/model/github_repo_ui_data.dart';
 import 'package:get/get.dart';
@@ -13,28 +14,41 @@ class HomeController extends BaseController {
   List<GithubRepoUiData> get repositoryList => _githubRepoController.toList();
 
   getGithubGetxRepositoryList() {
-    _repository
-        .searchRepository(
-      GithubSearchQueryParam(
-        searchKeyWord: 'flutter getx template',
-        perPage: 10,
-        pageNumber: 1,
+    callDataService(
+      _repository.searchRepository(
+        GithubSearchQueryParam(
+          searchKeyWord: 'flutter getx template',
+          perPage: 10,
+          pageNumber: 1,
+        ),
       ),
-    )
-        .then((response) {
-      List<GithubRepoUiData> repoList = [];
-      response.items?.forEach((element) {
-        var repo = GithubRepoUiData(
-            repositoryName: element.name != null ? element.name! : "Null",
-            ownerLoginName:
-                element.owner != null ? element.owner!.login! : "Null",
-            ownerAvatar: element.owner != null ? element.owner!.avatarUrl! : "",
-            numberOfStar: 10,
-            numberOfFork: 4);
-        repoList.add(repo);
-      });
-      _githubRepoController(repoList);
-      logger.d("Repo response: ${response.totalCount}");
+      onSuccess: _handleRepoListResponseSuccess,
+    );
+    // _repository
+    //     .searchRepository(
+    //   GithubSearchQueryParam(
+    //     searchKeyWord: 'flutter getx template',
+    //     perPage: 10,
+    //     pageNumber: 1,
+    //   ),
+    // )
+    //     .then((response) {
+    // });
+  }
+
+  _handleRepoListResponseSuccess(GithubRepoSearchResponse response) {
+    List<GithubRepoUiData> repoList = [];
+    response.items?.forEach((element) {
+      var repo = GithubRepoUiData(
+          repositoryName: element.name != null ? element.name! : "Null",
+          ownerLoginName:
+              element.owner != null ? element.owner!.login! : "Null",
+          ownerAvatar: element.owner != null ? element.owner!.avatarUrl! : "",
+          numberOfStar: 10,
+          numberOfFork: 4);
+      repoList.add(repo);
     });
+    _githubRepoController(repoList);
+    logger.d("Repo response: ${response.totalCount}");
   }
 }

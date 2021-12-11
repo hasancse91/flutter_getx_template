9a065f9682d5ecb5050e4e3ac90b382d5a10bd76
Shafayat <shafayathossainkhan@gmail.com>
2021-12-11 17:18:06 +0600

    Firebase integration ongoing. It's not initializing.

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '/app/my_app.dart';
 import '/flavors/build_config.dart';
 import '/flavors/env_config.dart';
 import '/flavors/environment.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   EnvConfig devConfig = EnvConfig(
     appName: "Flutter GetX Template Dev",
     baseUrl: "https://api.github.com",
@@ -16,6 +19,10 @@ void main() {
     envType: Environment.DEVELOPMENT,
     envConfig: devConfig,
   );
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "conf/.env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform(Environment.DEVELOPMENT),
+  );
   runApp(const MyApp());
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '/app/my_app.dart';
 import '/flavors/build_config.dart';
 import '/flavors/env_config.dart';
 import '/flavors/environment.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   EnvConfig prodConfig = EnvConfig(
     appName: "Flutter GetX Template Prod",
     baseUrl: "https://api.github.com",
@@ -15,6 +18,12 @@ void main() {
   BuildConfig.instantiate(
     envType: Environment.PRODUCTION,
     envConfig: prodConfig,
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "conf/.env");
+  await Firebase.initializeApp(
+    name: "flutter-getx-template",
+    options: DefaultFirebaseOptions.currentPlatform(Environment.PRODUCTION),
   );
 
   runApp(const MyApp());

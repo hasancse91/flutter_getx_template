aa8ea7e61dd9646a86fa3ba8918db299b7ee584c
tawhidularefin <tawhidularefin530@gmail.com>
2022-01-03 23:30:50 +0600

    Completed: Settings Page View

--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getx_template/app/core/values/app_values.dart';
 import 'package:flutter_getx_template/app/core/widget/paging_view.dart';
 import 'package:flutter_getx_template/app/core/widget/ripple.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '/app/core/base/base_view.dart';
 import '/app/core/values/text_styles.dart';
@@ -11,6 +13,8 @@ import '/app/modules/settings/controllers/settings_controller.dart';
 
 class SettingsView extends BaseView<SettingsController> {
   bool switchValue = true;
+  static const dp16 = 16.0;
+
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
     return CustomAppBar(
@@ -30,15 +34,23 @@ class SettingsView extends BaseView<SettingsController> {
           Padding(
             padding: const EdgeInsets.all(AppValues.padding),
             child: Ripple(
-              onTap: ()=>logger.d('Theme Clicked'),
+              onTap: ()=>Fluttertoast.showToast(
+                  msg: "Theme is clicked",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.black,
+                  textColor: Colors.white,
+                  fontSize: dp16,
+              ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children:  [
-                  const Icon(Icons.person),
-                  const SizedBox(width: 20),
-                  const Text('Theme', style: titleStyle),
-                  const Spacer(),
-                  Switch(value: switchValue, onChanged: (newValue)=>switchValue = newValue),
+                children: const  [
+                  Icon(FontAwesomeIcons.ethereum),
+                  SizedBox(width: 20),
+                  Text('Theme', style: titleStyle),
+                  Spacer(),
+                  Icon(Icons.arrow_forward_ios, color: Colors.grey),
                 ],
               ),
             ),
@@ -46,7 +58,15 @@ class SettingsView extends BaseView<SettingsController> {
           Padding(
             padding: const EdgeInsets.all(AppValues.padding),
             child: Ripple(
-                onTap: ()=>logger.d('Language clicked'),
+                onTap: ()=>Fluttertoast.showToast(
+    msg: "Language is clicked",
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.CENTER,
+    timeInSecForIosWeb: 1,
+    backgroundColor: Colors.black,
+    textColor: Colors.white,
+    fontSize: dp16,
+    ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
@@ -55,6 +75,30 @@ class SettingsView extends BaseView<SettingsController> {
                   Text('Language', style: titleStyle),
                   Spacer(),
                   Icon(Icons.arrow_forward_ios, color: Colors.grey)
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(AppValues.padding),
+            child: Ripple(
+              onTap: ()=>Fluttertoast.showToast(
+                  msg: "Font Size is clicked",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.CENTER,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.black,
+                  textColor: Colors.white,
+                  fontSize: dp16,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(FontAwesomeIcons.font),
+                  SizedBox(width: 20),
+                  Text('Font Size', style: titleStyle),
+                  Spacer(),
+                  Icon(Icons.arrow_forward_ios, color: Colors.grey),
                 ],
               ),
             ),

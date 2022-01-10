804a54c808e53b537da49c5460819e6b42dc0063
Tawhidul Arefin <tawhidularefin530@gmail.com>
2022-01-10 17:03:30 +0600

    Modified: settings view code

--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -2,33 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_template/app/core/values/app_values.dart';
 import 'package:flutter_getx_template/app/modules/settings/widgets/settings_widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import '/app/core/base/base_view.dart';
 import '/app/core/widget/custom_app_bar.dart';
 import '/app/modules/settings/controllers/settings_controller.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsView extends BaseView<SettingsController> {
-  List prefixIcons = [
-    FontAwesomeIcons.ethereum,
-    Icons.language,
-    FontAwesomeIcons.font
-  ];
-  List suffixIcons = [
-    Icons.arrow_forward_ios,
-    Icons.arrow_forward_ios,
-    Icons.arrow_forward_ios
-  ];
-  List titles = [
-    'Theme',
-    'Language',
-    'Font Size',
-  ];
-  List toasts = [
-    'Theme clicked',
-    'Language clicked',
-    'Font Size clicked',
-  ];
-
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
     return CustomAppBar(
@@ -42,11 +21,38 @@ class SettingsView extends BaseView<SettingsController> {
     return _getView();
   }
 
-  _getView() => ListView.separated(
-        itemBuilder: (context, index) => SettingsWidgets(toasts[index],
-            prefixIcons[index], suffixIcons[index], titles[index]),
-        separatorBuilder: (BuildContext context, int index) =>
-            const SizedBox(height: AppValues.smallMargin),
-        itemCount: 3,
+  _getView() => Column(
+        children: [
+          ItemSettings(
+            () => _show('Theme clicked'),
+            'ic_theme.svg',
+            'arrow_forward.png',
+            'Theme',
+          ),
+          ItemSettings(
+            () => _show('Language clicked'),
+            'ic_theme.svg',
+            'arrow_forward.png',
+            'Language',
+          ),
+          ItemSettings(
+            () => _show('Font Size clicked'),
+            'ic_theme.svg',
+            'arrow_forward.png',
+            'Font Size',
+          ),
+        ],
       );
+
+  _show(String msg) {
+    Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: AppValues.padding,
+    );
+  }
 }
--- lib/app/modules/settings/widgets/settings_widgets.dart
+++ lib/app/modules/settings/widgets/settings_widgets.dart
@@ -1,44 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_template/app/core/base/base_widget_mixin.dart';
+import 'package:flutter_getx_template/app/core/values/app_colors.dart';
 import 'package:flutter_getx_template/app/core/values/app_values.dart';
 import 'package:flutter_getx_template/app/core/values/text_styles.dart';
+import 'package:flutter_getx_template/app/core/widget/asset_image_view.dart';
 import 'package:flutter_getx_template/app/core/widget/ripple.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-class SettingsWidgets extends StatelessWidget {
-  final toast, prefixIcon, suffixIcon, title;
+class ItemSettings extends StatelessWidget with BaseWidgetMixin {
+  final String prefixImage;
+  final String suffixImage;
+  final String title;
+  final Function onTap;
 
-  const SettingsWidgets(
-      this.toast, this.prefixIcon, this.suffixIcon, this.title);
-
-  static const dp16 = 16.0;
+  ItemSettings(this.onTap, this.prefixImage, this.suffixImage, this.title);
 
   @override
-  Widget build(BuildContext context) {
+  Widget body(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(AppValues.padding),
       child: Ripple(
-          onTap: () => _show(toast),
-          child: Row(
-            children: [
-              Icon(prefixIcon),
-              const SizedBox(width: 10),
-              Text(title, style: titleStyle),
-              const Spacer(),
-              Icon(suffixIcon, color: Colors.grey),
-            ],
-          )),
-    );
-  }
-
-  void _show(String msg) {
-    Fluttertoast.showToast(
-      msg: msg,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      timeInSecForIosWeb: 1,
-      backgroundColor: Colors.black,
-      textColor: Colors.white,
-      fontSize: dp16,
+        onTap: () => onTap,
+        child: Row(
+          children: [
+            AssetImageView(
+              fileName: prefixImage,
+              height: AppValues.margin_20,
+              width: AppValues.margin_20,
+            ),
+            const SizedBox(width: 10),
+            Text(title, style: titleStyle),
+            // const Spacer(),
+            AssetImageView(
+              fileName: suffixImage,
+              color: AppColors.centerTextColor,
+              height: AppValues.margin_20,
+              width: AppValues.margin_20,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
